a3d1bfa85d6111b829eb9c8ff1d3b0224ee4ba6b
gepoliz <48353101+gepoliz@users.noreply.github.com>
2019-05-19 22:14:33 +0300

    Add files via upload

---
+++ ergasia3.sql
@@ -0,0 +1,7 @@
+ create table product(id int(11) NOT NULL AUTO_INCREMENT, 
+ 					  name VARCHAR(100) NOT NULL,
+    				  image varchar(100) NOT NULL,
+    				  price float NOT NULL,
+    				  stock int(100),PRIMARY KEY (id));
+
+    				  
