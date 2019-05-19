3dde981fbd08feb963d6ceb00dd5c6471ab2b562
gepoliz <48353101+gepoliz@users.noreply.github.com>
2019-05-19 22:24:10 +0300

    Delete ergasia3.sql

--- ergasia3.sql
+++
@@ -1,7 +0,0 @@
- create table product(id int(11) NOT NULL AUTO_INCREMENT, 
- 					  name VARCHAR(100) NOT NULL,
-    				  image varchar(100) NOT NULL,
-    				  price float NOT NULL,
-    				  stock int(100),PRIMARY KEY (id));
-
-    				  
